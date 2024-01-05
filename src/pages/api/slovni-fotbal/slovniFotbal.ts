8f613c62bed1e7d6a8ec7a57183a89e887155c28
romant04 <roman.tarnai.04@gmail.com>
2024-01-05 18:54:35 +0100

    moved slovnik

--- src/pages/api/slovni-fotbal/slovniFotbal.ts
+++ src/pages/api/slovni-fotbal/slovniFotbal.ts
@@ -8,10 +8,7 @@ export default async function slovniFotbal(
   const { word } = req.query;
 
   try {
-    const file = await fs.readFile(
-      process.cwd() + "/src/app/assets/slovni-fotbal/slovnik.txt",
-      "utf8"
-    );
+    const file = await fs.readFile("./slovnik.txt", "utf8");
     const data = file.split("\r\n").filter((slovo) => slovo == word).length > 0;
     return res.status(200).json(data);
   } catch (e) {
